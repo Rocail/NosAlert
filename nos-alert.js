fbb8d628f443b29597775929c9fd394e1cef4c26
Louis Clement <louis.clement@epitech.eu>
2018-06-25 20:50:51 +0200

    test

--- nos-alert.js
+++ nos-alert.js
@@ -11,8 +11,14 @@ const initStatus = {
         progress: "0",
         eventType: "0",
     },
+    currentDate: null,
     init: true
 };
+
+const clan = Object.freeze({
+    ANGE: "Ange",
+    DEMON: "Démon",
+});
 
 const initChannelIds = [];
 
@@ -82,13 +88,35 @@ exports.NosAlert = class NosAlert {
         this.client.channels.get(channelId).send(helpMessage);
     }
 
+
+    getPercents() {
+        let message = "Angels percents: " + this.status.angels.progress + "%\n"
+            + "Demons percents: " + this.status.demons.progress + "%";
+        return message;
+    }
+
     start() {
         setInterval(() => this.checkStatus(), 60 * 1000);
     }
 
+    checkStatus() {
+        this.getStatus()
+            .then((newStatus) => {
+                if (newStatus.angels.eventType === "3" && this.status.angels.eventType !== "3") {
+                    this.status = newStatus;
+                    this.alert(clan.ANGE);
+                } else if (newStatus.demons.eventType === "3" && this.status.demons.eventType !== "3") {
+                    this.status = newStatus;
+                    this.alert(clan.DEMON);
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
     getStatus(serverNumber = 4) {
         return new Promise((resolve, reject) => {
-            let url = "https://glaca.nostale.club/api/fr/" + serverNumber;
+            // let url = "https://glaca.nostale.club/api/fr/" + serverNumber;
+            let url = "https://glaca.nostale.club/api/de/2/";
 
             request(url, (err, res, body) => {
                 if (err !== null) {
@@ -106,35 +134,58 @@ exports.NosAlert = class NosAlert {
         })
     }
 
-    alert(message) {
+    alert(clan) {
         console.log("ALERT");
         for (let channel of this.client.channels) {
-            if (channel.name === "blabla-nostale" || channel.name === "act4") {
-                this.client.channels.get(this.channelIds[count]).send(message);
+            if (channel[1].type === "text") {
+                console.log(channel[1].name);
+//                 if (channel[1].name === "blabla-nostale" || channel[1].name === "act4") {
+                if (channel[1].name === "act4") {
+                    channel[1].send(this.displayStatus(clan));
+                }
             }
         }
     }
 
-    checkStatus() {
-        this.getStatus()
-            .then((newStatus) => {
-                if (newStatus.angels.eventType === "3"  && this.status.angels.eventType !== "3") {
-                    if (!this.status.init)
-                        this.alert("@everyone RAID ANGE");
-                } else if (newStatus.demons.eventType !== "3" && this.status.demons.eventType !== "3") {
-                    if (this.status.init)
-                        this.status.init = false;
-                    else
-                        this.alert("@everyone RAID DEMON");
+    displayStatus(clan) {
+        let status;
+        console.log(this.status);
+        if (clan === clan.ANGE) {
+            status = this.status.angels;
+        } else {
+            status = this.status.demons;
+        }
+        return (
+            {
+                embed: {
+                    color: 3447003,
+                    author: {
+                        name: this.client.user.username,
+                        icon_url: this.client.user.avatarURL
+                    },
+                    title: "Raid act4 !",
+                    fields: [
+                        {
+                            name: "Côté",
+                            value: clan
+                        },
+                        {
+                            name: "Début:",
+                            value: (new Date(Math.floor(1529952139376 / 1000) * 1000))
+                        },
+                    ]
                 }
-                this.status = newStatus;
-            })
-            .catch(err => console.log(err));
+            }
+        )
     }
 
-    getPercents() {
-        let message = "Angels percents: " + this.status.angels.progress + "%\n"
-            + "Demons percents: " + this.status.demons.progress + "%";
-        return message;
+    dateObjToStr(obj, spacing = true) {
+        let str = ""
+            + ("0" + obj.getDate()).slice(-2) + "/"
+            + ("0" + (obj.getMonth() + 1)).slice(-2) + "/"
+            + obj.getFullYear() + (spacing ? "\t" : " ")
+            + ("0" + obj.getHours()).slice(-2) + ":"
+            + ("0" + obj.getMinutes()).slice(-2);
+        return str;
     }
 };
