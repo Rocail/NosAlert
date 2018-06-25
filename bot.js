c946bf66623cde9cc798a311d084fedfc9155b60
Louis Clement <louis.clement@epitech.eu>
2018-06-25 19:09:56 +0200

    PUBLIC

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const NosAlert = require("./nos-alert").NosAlert;
-const config = require("config");
 
-let bot = new NosAlert("Mjk3MDkxMDQzNTUwMTY3MDQw.DhKlJA.Heh7dbdIkZNf4cjGtWvtfO7F9iY");
+let bot = new NosAlert(process.env.TOKEN);
 
 bot.start();
