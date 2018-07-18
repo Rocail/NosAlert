a2865ed12110508d7326807399e69e7cc130eae7
Louis Clement <louis.clement@epitech.eu>
2018-07-18 18:24:36 +0200

    starting things

--- bot.js
+++ bot.js
@@ -1,9 +1,24 @@
 const NosAlert = require("./nos-alert").NosAlert;
 
+
 let token = process.env.TOKEN;
+let db;
+
 if (typeof(token) === "undefined") {
-    token = require("./config/development").token;
+    let config = require("./config/development");
+    token = config.token;
+    db = config.db;
+} else {
+    db = {
+        url: PROCESS.env.DB_URL,
+        name: PROCESS.env.DB_NAME,
+        user: PROCESS.env.DB_USERNAME,
+        password: PROCESS.env.DB_USERPWD,
+    }
 }
+
+let url =
+
 
 let bot = new NosAlert(token);
 
